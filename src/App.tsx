2e990f7c0bd97a77490e30a1d64aa942d2b3ac21
Alberto Cangialosi <billyballo.res@gmail.com>
2024-01-30 23:17:33 +0100

    feat: 'npm install' new devDepencies and setup types annotation, plus removed StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { memo, useCallback, useEffect, useState } from "react";
+// import { MouseEvent, KeyboardEvent } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+	children?: any;
 }
+
+// assigning the type of the is FC functional component
+const App: React.FC<Props> = memo((props: Props) => {
+	const [isLogged, setIsLogged] = useState(false);
+
+	const [inputEmail, setInputEmail] = useState("");
+	const [users, setUsers] = useState<any>({});
+
+	// state of the page, what it shows
+	const [page, setPage] = useState<any>(<></>);
+
+	const onClickLogin = useCallback(() => {
+		setIsLogged(true);
+		localStorage.setItem("email", inputEmail);
+		if (!!users[inputEmail]) {
+			const newUsers = {
+				...users,
+				[inputEmail]: {
+					counter: users[inputEmail].counter + 1,
+					lastAccess: new Date().toISOString(),
+				},
+			};
+			setUsers(newUsers);
+			localStorage.setItem("users", JSON.stringify(newUsers));
+		} else {
+			const newUsers = {
+				...users,
+				[inputEmail]: {
+					counter: 1,
+					lastAccess: new Date().toISOString(),
+				},
+			};
+
+			setUsers(newUsers);
+			localStorage.setItem("users", JSON.stringify(newUsers));
+		}
+	}, [inputEmail, users]);
+
+	const onClickLogout = useCallback(() => {
+		setInputEmail("");
+		setIsLogged(false);
+		localStorage.removeItem("email");
+	}, []);
+
+	const onChangeEmail = useCallback(
+		(event: any) => setInputEmail(event.target.value),
+		[]
+	);
+
+	useEffect(
+		// on loading / mount, fetching data
+		() => {
+			console.log("didMount");
+
+			// Version 2.0
+			const cachedEmail: string | null = localStorage.getItem("email") || "";
+			const storedUsers: string = localStorage.getItem("users") || "";
+			const cachedUsers: Object = JSON.parse(storedUsers);
+
+			setIsLogged(!!cachedEmail);
+
+			setInputEmail(cachedEmail);
+			setUsers(cachedUsers);
+		},
+		[]
+	);
+
+	useEffect(
+		// when isLogged change
+		() => {
+			console.log("didUpdate");
+
+			const welcomePage = (
+				<>
+					{!!users[inputEmail] && users[inputEmail].counter > 1 ? (
+						<>
+							<div>Bentornat* {inputEmail}</div>
+							<div>
+								Ultimo accesso{" "}
+								{new Date(users[inputEmail].lastAccess).toLocaleString()}
+							</div>
+						</>
+					) : (
+						<div>Benvenut* {inputEmail}</div>
+					)}
+					<button onClick={onClickLogout}>Logout</button>
+				</>
+			);
+
+			const loginPage = (
+				<>
+					<input
+						placeholder="Inserisci email"
+						value={inputEmail}
+						onChange={onChangeEmail}
+					/>
+					<button onClick={onClickLogin} disabled={!inputEmail}>
+						Login
+					</button>
+				</>
+			);
+
+			setPage(isLogged ? welcomePage : loginPage);
+		},
+		[isLogged, users, inputEmail, onChangeEmail, onClickLogin]
+	);
+
+	return (
+		<section className="container-fluid d-flex flex-column justify-content-center align-content-center g-1 p-auto m-auto w-50 h-100">
+			{page}
+		</section>
+	);
+});
+
+App.propTypes = {
+	children: PropTypes.any,
+};
 
 export default App;
